2f942c04d9af784bf8044a23e2a04064a927811e
Prutfugl <Prutfugl789@gmail.com>
2025-04-28 11:33:47 +0200

    commit-2

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1600, 900);
+  background("grey")
+
+  DagensNyhed();
+  TidOgDato();
+
+  function DagensNyhed(){
+    fill("blue");
+    rect(0,0, 1600, 100);
+    fill("black");
+    textSize(50);
+    text("Nyhed: Galla starter snart, køb billetter nu!", 20, 60);
+  }
+
+  function TidOgDato(){
+    fill("blue")
+    rect(0,800,1600,100)
+    fill("black");
+    textSize(50);
+    text(day() + "/" + month() + "/" + year(), 20, 870);
+    text(hour() + ":" + minute() + ":" + second(), 700, 870);
+  }
+
+
 }
 
-function draw() {
-  background(220);
-}
