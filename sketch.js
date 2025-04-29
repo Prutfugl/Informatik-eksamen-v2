355ef07d3b03cb34c894fa4efe083eed3a4959ea
Prutfugl <Prutfugl789@gmail.com>
2025-04-29 10:37:26 +0200

    commit-6

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 
 let img;
+let img2;
 
 function preload() {
   img = loadImage("mad.png");
+  img2 = loadImage("Fest.png");
 }
 
 
@@ -16,6 +18,7 @@ function setup() {
   TidOgDato();
   Billede();
 
+  //Laver baren oppe i toppen, til visnin af dagens nyhed
   function DagensNyhed(){
     fill(65, 65, 65);
     rect(0,0, 1600, 100);
@@ -24,6 +27,7 @@ function setup() {
     text("Nyhed: Galla starter snart, køb billetter nu!", 20, 60);
   }
 
+  //laver baren nede i bunden, til visning af tid og dato
   function TidOgDato(){
     fill(65, 65, 65)
     rect(0,800,1600,100)
@@ -33,8 +37,10 @@ function setup() {
     text(hour() + ":" + minute() + ":" + second(), 700, 870);
   }
 
-
+  //Viser billede i midten af skærmen
   function Billede(){
+    image(img2, 0, 100, 1600, 700);
+    delaytime(20);
     image(img, 0, 100, 1600, 700);
     console.log("hello :)")
   }
