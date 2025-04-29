6f13979c07c83b083fba78c25953ad032dce63ea
Prutfugl <Prutfugl789@gmail.com>
2025-04-29 08:36:47 +0200

    commit-5

--- sketch.js
+++ sketch.js
@@ -1,23 +1,33 @@
+
+let img;
+
+function preload() {
+  img = loadImage("mad.png");
+}
+
+
+
 function setup() {
   createCanvas(1600, 900);
   background("grey")
+
 
   DagensNyhed();
   TidOgDato();
   Billede();
 
   function DagensNyhed(){
-    fill("blue");
+    fill(65, 65, 65);
     rect(0,0, 1600, 100);
-    fill("black");
+    fill("white");
     textSize(50);
     text("Nyhed: Galla starter snart, køb billetter nu!", 20, 60);
   }
 
   function TidOgDato(){
-    fill("blue")
+    fill(65, 65, 65)
     rect(0,800,1600,100)
-    fill("black");
+    fill("white");
     textSize(50);
     text(day() + "/" + month() + "/" + year(), 20, 870);
     text(hour() + ":" + minute() + ":" + second(), 700, 870);
@@ -25,9 +35,7 @@ function setup() {
 
 
   function Billede(){
-    let img
-    img = loadImage("Man-Happy.jpg");
-    image(img);
+    image(img, 0, 100, 1600, 700);
     console.log("hello :)")
   }
 
